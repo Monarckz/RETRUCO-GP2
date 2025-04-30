bd85f60fb6d08a34ecb0988532d6b6e3d3037799
Monarckz <Facundoalvarez854@gmail.com>
2025-04-30 15:19:02 -0300

    app.js

--- Punto 6/app.js
+++ Punto 6/app.js
@@ -1,4 +1,15 @@
-let num1 = Number(prompt("ingrese el primer numero:"));
-let num2 = Number(prompt("ingrese el segundo numero:"));
-num3 = num1 + num2
-alert("la suma es:" + num3)
+// Ejercicio Hecho por Alvarez Facundo
+
+
+let num1 = Number(prompt("Ingrese el primer numero:"))
+let num2 = Number(prompt("Ingrese el segundo numero:"))
+
+if (num1 > num2){
+    alert("el mayor es: " + num1)
+} if (num2 > num1){
+    alert("el mayor es: " + num5)
+} 
+
+else {
+    alert("ambos numeros son iguales: ")
+}
